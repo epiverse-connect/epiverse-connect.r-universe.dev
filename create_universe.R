863015011a9862b7285a3c33b0d5d964b53fd71b
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-06-12 10:58:24 +0200

    Remove incorrect repos and add subdir when needed

--- create_universe.R
+++ create_universe.R
@@ -1,3 +1,8 @@
+extra_opts <- tibble::tribble(
+  ~package, ~subdir,
+  "epitweetr", "epitweetr"
+)
+
 read.csv("https://raw.githubusercontent.com/cran-task-views/Epidemiology/main/data/source_repositories.csv") |>
   dplyr::rename(package = "package") |>
   dplyr::mutate(github_repo = ifelse(!is.na(github_repo), github_repo, glue::glue('cran/{package}'))) |>
@@ -5,4 +10,5 @@ read.csv("https://raw.githubusercontent.com/cran-task-views/Epidemiology/main/da
     url = glue::glue("https://github.com/{github_repo}"),
     .keep = "unused"
   ) |>
+  dplyr::full_join(extra_opts) |>
   jsonlite::write_json("packages.json", pretty = TRUE)
